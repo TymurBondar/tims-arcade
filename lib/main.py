48edaa17c520feb386c65618c9dd4a544d778e46
TymurBondar <tymurbondar@outlook.com>
2023-11-07 10:14:13 -0500

    start of day, working

--- lib/main.py
+++ lib/main.py
@@ -1,48 +1,56 @@
 import pygame
+import pygame_menu
 
 
 def main():
+
+    global selected_game 
+    selected_game = "Minesweeper"
+
+    def select_game(_, game):
+        global selected_game
+        selected_game = game
+
+    def launch_selected_game():
+        print(selected_game)
+        menu.disable()
+        return
+
     # Initialize Pygame
     pygame.init()
 
     # Set the width and height of the screen (width, height).
-    size = (700, 500)
+    size = (800, 600)
     screen = pygame.display.set_mode(size)
 
     # Set the title of the window
-    pygame.display.set_caption("My Game")
+    pygame.display.set_caption("Tims Arcade")
 
     # Loop until the user clicks the close button.
-    done = False
+
+    menu = pygame_menu.Menu("Welcome to Tims Arcade", 800, 600, theme=pygame_menu.themes.THEME_DARK)
+
+    menu.add.selector(
+            "game :",
+            [("Minesweeper", "Minesweeper"), ("TicTacToe", "TicTacToe"), ("Snake", "Snake")],
+            onchange=select_game,
+        )
+    menu.add.button("Play", launch_selected_game)
+    menu.mainloop(screen)
 
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
-
     # Main program loop
-    while not done:
-        # --- Main event loop
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-        # --- Game logic should go here
-
-        # --- Screen-clearing code goes here
-
-        # Here, we clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
-        screen.fill((34, 34, 34))
-
-        # --- Drawing code should go here
-
-        # --- Go ahead and update the screen with what we've drawn.
-        pygame.display.flip()
-
+    while True:
         # --- Limit to 60 frames per second
         clock.tick(60)
+        screen.fill((37, 37, 37))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.update()
 
-    # Close the window and quit.
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
